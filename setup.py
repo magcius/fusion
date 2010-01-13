4e619597cf4a3482786e4e8363070a2d1b179220
magcius <jstpierre@mecheye.net>
2010-01-13 18:22:46 -0500

    Change package name.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 
 from distutils.core import setup
 
-setup(name='mecheye-fusion-python',
+setup(name='mecheye-fusion',
       version='0.1',
       description='SWF export library',
       author='JP "magcius" St. Pierre, Josh Lory',
       author_email='jstpierre@mecheye.net, josh21409@gmail.com',
-      url='http://www.github.com/mecheye/mecheye-fusion-python',
+      url='http://github.com/mecheye/mecheye-fusion-python',
       packages=['mech','mech.fusion','mech.fusion.swf','mech.fusion.avm1','mech.fusion.avm2'])
