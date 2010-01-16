538970168a045c938f8a4151dc106a0b10d662d1
magcius <jstpierre@mecheye.net>
2010-01-15 19:52:44 -0500

    Update the repo URL.

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from distutils.core import setup
 
 setup(name='mecheye-fusion',
-      version='0.1',
+      version='0.1.1',
       description='SWF export library',
       author='JP "magcius" St. Pierre, Josh Lory',
       author_email='jstpierre@mecheye.net, josh21409@gmail.com',
-      url='http://github.com/mecheye/mecheye-fusion-python',
+      url='http://github.com/mecheye/mecheye-fusion',
       packages=['mech','mech.fusion','mech.fusion.swf','mech.fusion.avm1','mech.fusion.avm2'])
