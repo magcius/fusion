f3af8e416b250cf7c9cb663f1c3209f061dbcb05
Jasper St. Pierre <jstpierre@mecheye.net>
2010-11-24 07:48:08 -0500

    This commit intentionally left STUPID SETUPTOOLS!

--- mech/__init__.py
+++ mech/__init__.py
@@ -1,1 +1,7 @@
-# This file intentionally left blank.
+# This file intentionally left STUPID SETUPTOOLS!
+try:
+    from pkg_resources import declare_namespace as d
+    d('mech')
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
