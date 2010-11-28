19a8fbcf5099e657e2485f9f1adb7f94cc3212a2
Jasper St. Pierre <jstpierre@mecheye.net>
2010-11-28 04:28:00 -0500

    Release 0.2

--- mech/fusion/__init__.py
+++ mech/fusion/__init__.py
@@ -3,8 +3,8 @@ Mecheye Fusion, a SWF creator/parser in pure Python.
 """
 
 __version__  = "git"
-__released__ = "0.1.12"
+__released__ = "0.2"
 __author__   = 'Jasper St. Pierre, Josh Lory, Jon "Jonanin" Morton'
 __email__    = "jstpierre@mecheye.net"
-__url__      = "http://fusion.mecheye.net"
+__url__      = "https://github.com/mecheye/mecheye-fusion/"
 __license__  = "MPL 1.1"
